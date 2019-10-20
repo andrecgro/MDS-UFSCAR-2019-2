1751533e0d8436a3dc9f4cfc0a96919f6ae574b9
LorhanSohaky <lorhansohaky@gmail.com>
2019-10-20 12:40:06 -0300

    Arrumado warning

--- src/Components/Keepers/index.js
+++ src/Components/Keepers/index.js
@@ -5,7 +5,7 @@ import { Typography, Grid, GridList } from '@material-ui/core'
 
 export default function KeepersList () {
   return (
-    <GridList spacing={1} cols={1} container direction='row' className='gridList'>
+    <GridList spacing={1} cols={1} direction='row' className='gridList'>
 
       <Grid item sm={12}>
         <ServicesCard />
