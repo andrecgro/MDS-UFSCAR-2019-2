23d89b9d0094ee2a17b92b11c00e0f3e7f44cb54
LorhanSohaky <lorhansohaky@gmail.com>
2019-10-26 17:17:23 -0300

    Created route for login

--- src/Constants/routes.js
+++ src/Constants/routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import App from '../App'
 import Register from '../Pages/Register'
+import Main from '../Pages/Main'
 
 export default function Routes () {
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Register} />
+      <Route path='/' exact component={Main} />
+      <Route path='/register' exact component={Register} />
       <Route path='/home' exact component={App} />
     </BrowserRouter>
   )
