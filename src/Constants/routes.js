7e0400e74af0c3e7c68716f4d3512c0d98d64b13
LorhanSohaky <lorhansohaky@gmail.com>
2019-11-02 19:07:34 -0300

    Created route to Me component

--- src/Constants/routes.js
+++ src/Constants/routes.js
@@ -4,6 +4,7 @@ import App from '../App'
 import Register from '../Pages/Register'
 import Main from '../Pages/Main'
 import Orders from '../Components/Orders'
+import Me from '../Pages/Me'
 import HistoryOrders from '../Components/Orders/HistoricoPedidos'
 import FutureOrders from '../Components/Orders/PedidosAgendados'
 
@@ -16,6 +17,7 @@ export default function Routes () {
       <Route path='/orders' exact component={Orders} />
       <Route path='/scheduled' exact component={FutureOrders} />
       <Route path='/history' exact component={HistoryOrders} />
+      <Route path='/me' exact component={Me} />
     </BrowserRouter>
   )
 }
