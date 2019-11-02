0199529f40d64bf3157f8fb5ea7da82295ee1a6f
LorhanSohaky <lorhansohaky@gmail.com>
2019-11-02 20:03:10 -0300

    Added avatar

--- src/Pages/Me/index.js
+++ src/Pages/Me/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Box, AppBar, Toolbar, IconButton, Typography, Container, Grid, Avatar } from '@material-ui/core'
 import { ArrowBack } from '@material-ui/icons'
 
-export default function Me () {
+export default function Me ({ history }) {
   return (
     <Box>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton edge='start' color='inherit'>
+          <IconButton edge='start' color='inherit' onClick={() => history.push('/home')}>
             <ArrowBack />
           </IconButton>
           <Typography variant='h6'>
@@ -17,9 +17,8 @@ export default function Me () {
       </AppBar>
       <Container>
         <Grid container justify='center' alignItems='center'>
-          <Avatar alt='My name' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK1J-i3ZrWnicHdauDJ1ZUN9laTSlb1xG0bhlf9a484BGIL2JL' />
+          <Avatar alt='My name' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK1J-i3ZrWnicHdauDJ1ZUN9laTSlb1xG0bhlf9a484BGIL2JL' style={{ width: '300px', height: '300px' }} />
         </Grid>
-
       </Container>
     </Box>
   )
