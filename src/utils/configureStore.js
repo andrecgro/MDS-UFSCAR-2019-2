43b8069f2ec1ca738ccdbc9cf0a71261faa1a4d1
LorhanSohaky <lorhansohaky@gmail.com>
2019-11-03 19:48:44 -0300

    Adicionado redux thunk e logger

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+
+import { createLogger } from 'redux-logger'
+
+import thunk from 'redux-thunk'
+
 import rootReducer from '../reducer/rootReducer'
 
 const persistConfig = {
@@ -8,10 +14,12 @@ const persistConfig = {
   storage
 }
 
+export const logger = createLogger({})
+
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export default () => {
-  const store = createStore(persistedReducer)
+  const store = createStore(persistedReducer, applyMiddleware(thunk, logger))
   const persistor = persistStore(store)
   return { store, persistor }
 }
