011de1c7cc4475c85abe35151bc7476d6525d8b8
LorhanSohaky <lorhansohaky@gmail.com>
2019-11-03 18:01:46 -0300

    Removida barra superior

--- src/Pages/Me/index.js
+++ src/Pages/Me/index.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { Box, AppBar, Toolbar, IconButton, Typography, Container, Grid, Avatar } from '@material-ui/core'
-import { ArrowBack } from '@material-ui/icons'
+import {
+  Box,
+  Typography,
+  Container,
+  Grid,
+  Avatar
+} from '@material-ui/core'
 
 export default function Me ({ history }) {
   return (
     <Box>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton edge='start' color='inherit' onClick={() => history.push('/home')}>
-            <ArrowBack />
-          </IconButton>
-          <Typography variant='h6'>
-                      Minha Conta
+      <Container style={{ marginTop: '10px' }}>
+        <Grid container justify='center' alignItems='center' direction='column' style={{ flexGrow: 1 }}>
+          <Avatar
+            alt='My name'
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK1J-i3ZrWnicHdauDJ1ZUN9laTSlb1xG0bhlf9a484BGIL2JL'
+            style={{ width: '200px', height: '200px' }}
+          />
+          <Typography variant='h4' component='h1'>
+            Meu Nome
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <Grid container justify='center' alignItems='center'>
-          <Avatar alt='My name' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK1J-i3ZrWnicHdauDJ1ZUN9laTSlb1xG0bhlf9a484BGIL2JL' style={{ width: '300px', height: '300px' }} />
+          <Typography variant='body1'>
+            email@test.com
+          </Typography>
+          <Typography variant='body2' align='center' color='secondary'>
+            Versão 1.0.0
+          </Typography>
         </Grid>
       </Container>
     </Box>
